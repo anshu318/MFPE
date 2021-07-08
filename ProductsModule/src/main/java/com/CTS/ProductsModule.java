3b49ad54bce4afcec4429c1776e33acee50eccd0
anshu318 <anshumanpanda318@gmail.com>
2021-07-08 18:19:10 +0530

    mfpe

--- ProductsModule/src/main/java/com/CTS/ProductsModule.java
+++ ProductsModule/src/main/java/com/CTS/ProductsModule.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import lombok.extern.slf4j.Slf4j;
+
 @EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class ProductsModule {
 
 	public static void main(String[] args) {
+		log.info("Main started...");
 		SpringApplication.run(ProductsModule.class, args);
+		log.info("Main ended...");
 	}
 
 }
