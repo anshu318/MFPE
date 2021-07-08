1ddbb19c16fd8a7fde9fb3550acad41f44ad6168
anshu318 <anshumanpanda318@gmail.com>
2021-07-08 11:22:59 +0530

    mfpe

---
+++ Eureka-Server/src/test/java/com/cognizant/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cognizant.eureka;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
